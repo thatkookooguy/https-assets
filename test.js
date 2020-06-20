fa3c432a58c40313963ad169739a08f6fa5417d2
Neil Kalman <Neilkalman@gmail.com>
2020-06-21 00:21:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,56 +1,53 @@
 (function createInlineDiagram() {
-  console.log('i am alived!');
   let elementBeingDragged; // GLOBAL REFERENCE FOR ELEMENT BEING DRAGGED
   let startY, startHeight; // RESIZABLE DIV HELPERS
-
-    console.log('dom is loaded');
     injectStyleToHeader();
     const diagramContainer = createDiagramBox();
     createTreeCopyRecursive(document.body, diagramContainer);
     document.body.appendChild(diagramContainer);
 
   function createDiagramBox() {
-    const diagramBox = document.createElement('div');
-    diagramBox.id = 'kb-diagram';
-    diagramBox.innerHTML = '';
+    const diagramBox = document.createElement("div");
+    diagramBox.id = "kb-diagram";
+    diagramBox.innerHTML = "";
     // resizer
-    const resizer = document.createElement('div');
-    resizer.className = 'resizer';
+    const resizer = document.createElement("div");
+    resizer.className = "resizer";
     diagramBox.appendChild(resizer);
-    resizer.addEventListener('mousedown', initResize, false);
+    resizer.addEventListener("mousedown", initResize, false);
 
-    diagramBox.addEventListener('drop', onDrop, false);
+    diagramBox.addEventListener("drop", onDrop, false);
 
     return diagramBox;
   }
 
   function createTreeCopyRecursive(node, newParent) {
-    if (node.id === 'kb-diagram' || node.nodeName === 'SCRIPT') {
+    if (node.id === "kb-diagram" || node.nodeName === "SCRIPT") {
       return;
     }
 
-    const newElement = document.createElement('div');
-    newElement.classList.add('kb-element-rep');
-    const children = document.createElement('div');
-    children.classList.add('children');
+    const newElement = document.createElement("div");
+    newElement.classList.add("kb-element-rep");
+    const children = document.createElement("div");
+    children.classList.add("children");
     newElement.innerHTML = `<div class="kb-title">${node.nodeName}<div/>`;
     newElement.appendChild(children);
-    newElement.setAttribute('kb-name', node.nodeName);
+    newElement.setAttribute("kb-name", node.nodeName);
     newElement.originalNode = node;
 
-    if (node.nodeName !== 'BODY') {
-      newElement.addEventListener('dragstart', dragStart);
+    if (node.nodeName !== "BODY") {
+      newElement.addEventListener("dragstart", dragStart);
       // newElement.addEventListener('dragend', dragEnd);
-      newElement.setAttribute('draggable', true);
-      newElement.addEventListener('dragover', onDragOver);
-      newElement.addEventListener('dragleave', dragLeave);
+      newElement.setAttribute("draggable", true);
+      newElement.addEventListener("dragover", onDragOver);
+      newElement.addEventListener("dragleave", dragLeave);
     }
 
     newElement.style.backgroundColor = `#${str2RGB(node.nodeName)}`;
     newParent.appendChild(newElement);
 
-    newElement.addEventListener('mouseover', onMouseOver, false);
-    newElement.addEventListener('mouseout', onMouseOut, false);
+    newElement.addEventListener("mouseover", onMouseOver, false);
+    newElement.addEventListener("mouseout", onMouseOut, false);
 
     if (node.children.length) {
       createTreeCopyRecursive(node.children[0], children);
@@ -62,7 +59,7 @@
   }
 
   function str2RGB(str) {
-    return intToARGB(hashCode(str + 'hello'));
+    return intToARGB(hashCode(str + "hello"));
 
     function hashCode(str) {
       var hash = 0;
@@ -78,29 +75,29 @@
         ((i >> 16) & 0xff).toString(16) +
         ((i >> 8) & 0xff).toString(16) +
         (i & 0xff).toString(16);
-      hex += '000000';
+      hex += "000000";
       return hex.substring(0, 6);
     }
   }
 
   function onMouseOver() {
-    this.originalNode && this.originalNode.classList.add('kb-hover');
+    this.originalNode && this.originalNode.classList.add("kb-hover");
   }
 
   function onMouseOut() {
-    this.originalNode && this.originalNode.classList.remove('kb-hover');
+    this.originalNode && this.originalNode.classList.remove("kb-hover");
   }
 
   function onDragOver(event) {
     if (event.target === elementBeingDragged) {
-      event.target.style.border = '';
+      event.target.style.border = "";
       return;
     }
 
     // prevent default to allow drop
     event.preventDefault();
 
-    if (event.target.className !== 'kb-element-rep') {
+    if (event.target.className !== "kb-element-rep") {
       return;
     }
 
@@ -109,13 +106,13 @@
     const xInElement = mouseX - rect.left;
 
     if (xInElement < 10) {
-      event.target.style.border = '';
-      event.target.style.borderLeft = '5px solid black';
+      event.target.style.border = "";
+      event.target.style.borderLeft = "5px solid black";
     } else if (rect.width - xInElement < 10) {
-      event.target.style.border = '';
-      event.target.style.borderRight = '5px solid black';
+      event.target.style.border = "";
+      event.target.style.borderRight = "5px solid black";
     } else {
-      event.target.style.border = '5px solid black';
+      event.target.style.border = "5px solid black";
     }
   }
 
@@ -125,8 +122,8 @@
 
   function dragLeave(event) {
     // reset background of potential drop target when the draggable element leaves it
-    if (event.target.className == 'kb-element-rep') {
-      event.target.style.border = '';
+    if (event.target.className == "kb-element-rep") {
+      event.target.style.border = "";
     }
   }
 
@@ -134,51 +131,51 @@
     // prevent default action
     event.preventDefault();
     const target = event.target;
-    if (target.className == 'kb-element-rep') {
+    if (target.className == "kb-element-rep") {
       if (target.style.borderLeft) {
-        moveElement('left', elementBeingDragged, target);
+        moveElement("left", elementBeingDragged, target);
         moveElement(
-          'left',
+          "left",
           elementBeingDragged.originalNode,
           target.originalNode
         );
       }
 
       if (target.style.borderRight) {
-        moveElement('right', elementBeingDragged, target);
+        moveElement("right", elementBeingDragged, target);
         moveElement(
-          'right',
+          "right",
           elementBeingDragged.originalNode,
           target.originalNode
         );
       }
 
       if (event.target.style.border) {
-        moveElement('in', elementBeingDragged, target);
+        moveElement("in", elementBeingDragged, target);
         moveElement(
-          'in',
+          "in",
           elementBeingDragged.originalNode,
           target.originalNode
         );
       }
-      event.target.style.border = '';
+      event.target.style.border = "";
     }
   }
 
   function moveElement(position, node2move, nodeTarget) {
-    if (position === 'left') {
+    if (position === "left") {
       node2move.parentNode.removeChild(node2move);
       nodeTarget.parentNode.insertBefore(node2move, nodeTarget);
       return;
     }
 
-    if (position === 'right') {
+    if (position === "right") {
       node2move.parentNode.removeChild(node2move);
       nodeTarget.parentNode.insertBefore(node2move, nodeTarget.nextSibling);
       return;
     }
 
-    if (position === 'in') {
+    if (position === "in") {
       node2move.parentNode.removeChild(node2move);
       nodeTarget.appendChild(node2move);
       return;
@@ -188,20 +185,20 @@
   function initResize(e) {
     startY = e.clientY;
     startHeight = parseInt(
-      document.defaultView.getComputedStyle(box).height,
+      document.defaultView.getComputedStyle(diagramContainer).height,
       10
     );
-    document.documentElement.addEventListener('mousemove', doResize, false);
-    document.documentElement.addEventListener('mouseup', stopResize, false);
+    document.documentElement.addEventListener("mousemove", doResize, false);
+    document.documentElement.addEventListener("mouseup", stopResize, false);
   }
 
   function doResize(e) {
-    box.style.height = startHeight - e.clientY + startY + 'px';
+    box.style.height = startHeight - e.clientY + startY + "px";
   }
 
   function stopResize(e) {
-    document.documentElement.removeEventListener('mousemove', doResize, false);
-    document.documentElement.removeEventListener('mouseup', stopResize, false);
+    document.documentElement.removeEventListener("mousemove", doResize, false);
+    document.documentElement.removeEventListener("mouseup", stopResize, false);
   }
 
   function injectStyleToHeader() {
@@ -252,9 +249,9 @@
 }
 `;
 
-    const head = document.head || document.getElementsByTagName('head')[0];
-    const style = document.createElement('style');
-    style.type = 'text/css';
+    const head = document.head || document.getElementsByTagName("head")[0];
+    const style = document.createElement("style");
+    style.type = "text/css";
 
     style.innerHTML = css;
 
