2ef920ad97a1a34095ef5c9112ff752b4c29fee0
Neil Kalman <Neilkalman@gmail.com>
2020-09-13 05:36:11 +0300

    Update add-blur-to-cards.js

--- add-blur-to-cards.js
+++ add-blur-to-cards.js
@@ -1,17 +1,4 @@
-addStyle(`
-  ha-card {
-    backdrop-filter: blur(5px);
-  }
-`);
+const sheet = new CSSStyleSheet();
 
-
-function addStyle(styleString) {
-  const style = document.createElement('style');
-  style.textContent = styleString;
-  document.head.append(style);
-  
-  setTimeout(() => {
-    console.log('test selector');
-    console.log(document.queryselectorall('ha-card'));
-  }, 3000);
-}
+// replace all styles synchronously:
+sheet.replaceSync('ha-card { color: red; }');
