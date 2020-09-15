26ad3952a632d2df10117d79a6d3e72f2404f381
Neil Kalman <Neilkalman@gmail.com>
2020-09-15 13:32:55 +0300

    Update add-blur-to-cards.js

--- add-blur-to-cards.js
+++ add-blur-to-cards.js
@@ -1,4 +1,4 @@
 setTimeout(() => {
   const haCard = customElements.get('ha-card');
-  haCard._styles[0].styleSheet.addRule(':host', 'color: pink !important;', 0)
+  haCard._styles[0].styleSheet.addRule(':host', 'backdrop-filter: blur(5px)', 0)
 }, 1000);
