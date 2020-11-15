d78b77439a714902fff9b39118a06a9fa2e82ae4
Neil Kalman <Neilkalman@gmail.com>
2020-11-15 18:18:06 +0200

    Update add-blur-to-cards.js

--- add-blur-to-cards.js
+++ add-blur-to-cards.js
@@ -1,4 +1,8 @@
 setTimeout(() => {
   const haCard = customElements.get('ha-card');
-  haCard._styles[0].styleSheet.addRule(':host', 'backdrop-filter: blur(5px)', 0)
+  const haInfoDialog = customElements.get('ha-more-info-dialog');
+  haCard._styles[0].styleSheet.addRule(':host', 'backdrop-filter: blur(5px)', 0);
+  if (haInfoDialog && haInfoDialog._styles) {
+    haInfoDialog._styles[0].styleSheet.addRule(':host', 'backdrop-filter: blur(5px)', 0);
+  }
 }, 1000);
